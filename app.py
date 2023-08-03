c0c5b40bd826d35f866e7589ff0671b64977b2a3
zecko8 <58081922+zecko8@users.noreply.github.com>
2023-08-03 15:52:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
-  return "ciao"
+    return "Primo applicativo"
+
+
+@app.route("/file")
+def read_file():
+    f = open("./data/testfile.txt")
+    contents = f.read()
+    return(contents)
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
